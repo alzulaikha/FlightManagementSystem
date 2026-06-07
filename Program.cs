853e1ae40dec55caccbb7ac4e17b0cab5715e5b6
alzulaikha <alzulaikha1992@gmail.com>
2026-06-07 11:09:56 +0400

    added Shared Variables

--- Program.cs
+++ Program.cs
@@ -2,6 +2,35 @@
 {
     internal class Program
     {
+        List<string> passengerNames = new List<string>()
+        {"Ali","Maryam","Bader","Omar","Noor"};
+        List<string> ticketNumbers = new List<string>() { "TKT-100", "TKT-200", "TKT-300", "TKT-400", "TKT-500" };
+        string[] flightNumbers = { "OA101", "OA102", "OA103", "OA104", "OA105", "OA106" };
+        List<string> availableDates = new List<string>() { "06-03-2026", "28-04-2026", "19-05-2026", "02-06-2026" };
+        Dictionary<string, string> bookingRecord = new Dictionary<string, string>()
+        {
+            { "TKT-100","OA101|06-03-2026" },
+            { "TKT-200","OA102|28-04-2026" },
+            { "TKT-300","OA103|19-05-2026" },
+            { "TKT-400","OA104|02-06-2026" },
+            { "TKT-500","OA105|02-06-2026" },
+        };
+
+        Queue<string> checkedInQueue = new Queue<string>(new string[] { "Ali", "Maryam", "Bader", "Omar", "Noor" });
+        Stack<string> boardingStack = new Stack<string>();
+        List<string> cancelledTickets = new List<string>();
+        Dictionary<string, string> passengerSeatMap = new Dictionary<string, string>()
+            {
+            {"Ali","14A"},
+            {"Maryam","15A"},
+            { "Bader","16A"},
+            { "Omar","17A"},
+            { "Noor","18A"}
+        };
+        Queue<string> waitlistQueue = new Queue<string>();
+
+
+
 
         public static void mainMenu()
         {
@@ -18,6 +47,8 @@
             Console.WriteLine("10. Manage Waitlist & Seat Assignment");
             Console.WriteLine("0. Exit");
         }
+      
+              
         static void Main(string[] args)
         {
 
