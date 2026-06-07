f81019803ebb9075495b1d097892c7c1f4e50945
alzulaikha <alzulaikha1992@gmail.com>
2026-06-07 08:56:50 +0400

    added main menu function and switch cases

--- Program.cs
+++ Program.cs
@@ -2,9 +2,70 @@
 {
     internal class Program
     {
+
+        public static void mainMenu()
+        {
+            Console.WriteLine("SKY WINGS FLIGHT MANAGEMENT SYSTEM");
+            Console.WriteLine("1. Register New Passenger");
+            Console.WriteLine("2. View All Passengers");
+            Console.WriteLine("3. Book a Flight Ticket");
+            Console.WriteLine("4. View Booking Details");
+            Console.WriteLine("5. Update a Booking");
+            Console.WriteLine("6. Cancel a Ticket");
+            Console.WriteLine("7. Passenger Check-In");
+            Console.WriteLine("8. Board Passengers (Boarding Stack)");
+            Console.WriteLine("9. Generate Flight Manifest");
+            Console.WriteLine("10. Manage Waitlist & Seat Assignment");
+            Console.WriteLine("0. Exit");
+        }
         static void Main(string[] args)
         {
-            
-        }
+
+
+            bool exit = false;
+            while (exit == false)
+
+            {
+                mainMenu();
+                Console.WriteLine("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 0:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("invalid option please try again");
+                        break;
+
+
+
+                }
+                Console.WriteLine("press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+                }
     }
 }
